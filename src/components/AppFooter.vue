3b86423b02b26572eb53b9dcd4fdc9d23108acbf
giovannitamburini <giovatambu@gmail.com>
2023-03-28 18:35:46 +0200

    added basic structure and API

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,11 @@
 <script>
 
+import { store } from "../store.js";
+
 export default {
     data() {
         return {
-
+            store,
         }
     },
 }
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,31 @@
 <script>
 
+import { store } from "../store.js";
+import axios from "axios";
+
 export default {
     data() {
         return {
 
+            listCards: [],
+
+            store,
         }
+    },
+    created() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res) => {
+
+            console.log(res.data.data);
+            this.store.listCards = res.data.data
+        })
     },
 }
 </script>
 
 <template>
-    <div></div>
+    <div id="cards-list">
+
+    </div>
 </template>
 
 <style lang="scss" scoped></style>

7ad9eb591024e203d9dafac952531d6e4af327a8
giovannitamburini <giovatambu@gmail.com>
2023-03-28 16:19:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+
+import AppNav from "./components/AppNav.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppNav,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+  <AppNav></AppNav>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
+</template>
+
+<style lang="scss" scoped></style>

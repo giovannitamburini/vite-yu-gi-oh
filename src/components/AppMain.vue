93a15da204cb953c99e9c99b97434cf39f025b10
giovannitamburini <giovatambu@gmail.com>
2023-03-28 20:11:51 +0200

    completed ex with bonus

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,29 +3,49 @@
 import { store } from "../store.js";
 import axios from "axios";
 
+import CardItem from "./CardItem.vue";
+
 export default {
     data() {
         return {
 
-            listCards: [],
+            // listCards: [],
 
             store,
         }
     },
+    components: {
+        CardItem,
+    },
+
     created() {
         axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res) => {
 
-            console.log(res.data.data);
-            this.store.listCards = res.data.data
+            this.store.listCards = res.data.data;
         })
     },
 }
 </script>
 
 <template>
-    <div id="cards-list">
+    <div id="main">
+        <div v-if="this.store.listCards.length < 50" id="loading">
+            i'm working HARD for you
+        </div>
 
+        <div v-else id="cards-list">
+            <CardItem v-for="card in store.listCards" :cardYu="card"></CardItem>
+        </div>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#main {
+    #cards-list {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        gap: 5px;
+    }
+}
+</style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,67 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    props: {
+        cardYu: Object,
+    },
+
+}
+</script>
+
+<template>
+    <div class="card-yugioh-container">
+        <img :src="cardYu.card_images[0].image_url" alt="Yu-gi-oh card">
+        <div class="description-card">
+            <div>
+                <strong>name: </strong><em>{{ cardYu.name }}</em>
+            </div>
+            <div>
+                <strong>race: </strong><em>{{ cardYu.race }}</em>
+            </div>
+            <div>
+                <strong>archetype: </strong><em>{{ cardYu.archetype }}</em>
+            </div>
+            <div>
+                <strong>id: </strong><em>{{ cardYu.card_images[0].id }}</em>
+            </div>
+            <div class="prices">
+                <ul>
+                    <strong>PRICE:</strong>
+                    <li><strong>cardmarket: </strong><em>{{ cardYu.card_prices[0].cardmarket_price }}</em></li>
+                    <li><strong>tcgplayer: </strong><em>{{ cardYu.card_prices[0].tcgplayer_price }}</em></li>
+                    <li><strong>ebay: </strong><em>{{ cardYu.card_prices[0].ebay_price }}</em></li>
+                    <li><strong>amazon: </strong><em>{{ cardYu.card_prices[0].amazon_price }}</em></li>
+                    <li><strong>coolstuffinc: </strong><em>{{ cardYu.card_prices[0].coolstuffinc_price }}</em></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card-yugioh-container {
+    display: flex;
+    flex-flow: row nowrap;
+    gap: 5px;
+    padding: 5px;
+    border: 2px solid black;
+    width: 380px;
+
+    img {
+        width: 150px;
+    }
+
+    .prices {
+        ul {
+            padding: 0;
+            list-style-type: none;
+        }
+    }
+}
+</style>
